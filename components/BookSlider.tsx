e6e1e3ec959aedf51797cba845ab22e9e7cfd29f
WesleyLiaw <wesleyliaw777@gmail.com>
2023-06-19 11:18:00 -0500

    sliders fixed with some parent flexing

--- components/BookSlider.tsx
+++ components/BookSlider.tsx
@@ -8,19 +8,30 @@ interface BookSliderProps {
 
 export default function BookSlider({ books }: BookSliderProps) {
   return (
-    <div className="flex w-full snap-x overflow-x-auto gap-x-6">
+    <div className="mt-4 flex w-full snap-x gap-x-6 overflow-x-auto">
       {books?.map((book) => (
-        <Link
-        href={`/`}
-        key={book.id}
-        className="flex flex-shrink-0 snap-start flex-col">
-          <Image
-            src={`${book.imageLink}`}
-            width={172}
-            height={172}
-            alt="book image"
-          />
-          <span>xd123</span>
+        <Link href={`/book/${book.id}`} key={book.id}>
+          <div
+          className="relative flex w-[200px] h-full snap-start flex-col gap-y-2 rounded px-5 pb-4 pt-8 hover:bg-[#f1f6f4]">
+            <Image
+              src={`${book.imageLink}`}
+              width={172}
+              height={172}
+              alt="book image"
+            />
+            {book.subscriptionRequired && (
+              <div className="absolute right-2 top-2 flex h-[18px] items-center rounded-[20px] bg-[#032b41] px-2 text-[10px] text-white">
+                Premium
+              </div>
+            )}
+            <span className="font-bold leading-[18px] text-[#032b41]">
+              {book.title}
+            </span>
+            <span className="text-sm font-light text-[#6b757b]">
+              {book.author}
+            </span>
+            <span className="text-sm text-[#394547]">{book.subTitle}</span>
+          </div>
         </Link>
       ))}
     </div>
